6f16789460db8f5f6f015678c0ae48121c68a5aa
allelbhagya <allel.bhagya@gmail.com>
2024-01-17 20:48:08 +0530

    logout1

--- api/index.js
+++ api/index.js
@@ -86,8 +86,8 @@ app.get('/profile', (req,res) => {
   });
 });
 
-app.post('/logout', (req, res) => {
-  res.clearCookie('token').header('Cache-Control', 'no-store, max-age=0').send('ok');
+app.post('/logout', (req,res) => {
+  res.cookie('token', '').json('ok');
 });
 
 app.post('/log', upload.none(), async (req, res) => {
