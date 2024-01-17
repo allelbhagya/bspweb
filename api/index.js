a85fcbff421cda880abd8500cfecedb50f1b127b
allelbhagya <allel.bhagya@gmail.com>
2024-01-17 20:30:05 +0530

    logout

--- api/index.js
+++ api/index.js
@@ -86,8 +86,8 @@ app.get('/profile', (req,res) => {
   });
 });
 
-app.post('/logout', (req,res) => {
-  res.cookie('token', '').json('ok');
+app.post('/logout', (req, res) => {
+  res.clearCookie('token').json('ok');
 });
 
 app.post('/log', upload.none(), async (req, res) => {
