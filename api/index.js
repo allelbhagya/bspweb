f4b2c507d49845f10786fb90f00fc0f38d3626c2
allelbhagya <allel.bhagya@gmail.com>
2024-01-17 13:49:11 +0530

    api 1.1

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const User = require('./models/User');
-const Logs = require('./models/Logs')
+const Logs = require('./models/Logs');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const multer = require('multer');
@@ -10,16 +10,15 @@ const upload = multer();
 const cookieParser = require('cookie-parser');
 const app = express();
 
-
 const salt = bcrypt.genSaltSync(10);
 const secret = "qddi10eu90ikj1wqmn";
 
 const allowedOrigins = ['https://bspweb-client.vercel.app', 'https://bspweb-client-6bl1o3f3x-allelbhagya.vercel.app'];
+
 app.use(cors({
   methods: ["POST", "GET"],
   credentials: true,
   origin: function (origin, callback) {
-    // Check if the request origin is in the allowedOrigins array
     if (!origin) return callback(null, true);
     if (allowedOrigins.indexOf(origin) === -1) {
       const msg = 'The CORS policy for this site does not allow access from the specified origin.';
@@ -28,125 +27,137 @@ app.use(cors({
     return callback(null, true);
   }
 }));
+
 app.use(express.json());
 app.use(cookieParser());
 
-app.get("/", (req,res)=>{
-    res.json("ok works");
-})
+app.get("/", (req, res) => {
+  res.json("ok works");
+});
 
 mongoose.connect("mongodb+srv://bsp:bsp@bsp.liemt4a.mongodb.net/?retryWrites=true&w=majority");
 
-app.post('/register', async(req,res)=>{
-    const {username, password} = req.body;
-    try{
-        const userDoc = await User.create({
-            username,
-            password: bcrypt.hashSync(password, salt)})
-        res.json(userDoc);
-    }catch(e){
-        res.status(400).json(e);
-    } 
-})
-
+app.post('/register', async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const userDoc = await User.create({
+      username,
+      password: bcrypt.hashSync(password, salt)
+    })
+    res.json(userDoc);
+  } catch (e) {
+    res.status(400).json(e);
+  }
+});
 
 app.post('/login', async (req, res) => {
-    const { username, password } = req.body;
-    const userDoc = await User.findOne({ username });
+  const { username, password } = req.body;
+  const userDoc = await User.findOne({ username });
 
-    if (!userDoc) {
-        return res.status(400).json({ error: 'User not found' });
-    }
+  if (!userDoc) {
+    return res.status(400).json({ error: 'User not found' });
+  }
 
-    const passOk = bcrypt.compareSync(password, userDoc.password);
+  const passOk = bcrypt.compareSync(password, userDoc.password);
 
-    if (passOk) {
-        jwt.sign({ username, id: userDoc._id }, secret, { expiresIn: '1h' }, (err, token) => {
-            if(err) throw err;
-            res.cookie('token', token).json({
-                id:userDoc._id,
-                username,
-            });
-        });
-        //res.json();
-    } else {
-        return res.status(400).json({ error: 'Wrong credentials' });
-    }
-});
-
-
-app.get('/profile', (req,res) => {
-    const {token} = req.cookies;
-    jwt.verify(token, secret, {}, (err,info) => {
+  if (passOk) {
+    jwt.sign({ username, id: userDoc._id }, secret, { expiresIn: '1h' }, (err, token) => {
       if (err) throw err;
-      res.json(info);
+      res.cookie('token', token).json({
+        id: userDoc._id,
+        username,
+      });
     });
-  });  
-
-app.post('/logout', (req,res)=>{
-    res.cookie('token', '').json('ok');
-})
-
-app.post('/log', upload.none(), async(req, res) => {
-    const {time, duration, region, sensorID, stoppage, profile, comment, measure } = req.body;
-    const logDoc = await Logs.create({
-        time,
-        duration, 
-        region, 
-        sensorID, 
-        stoppage, 
-        profile, 
-        comment, 
-        measure
-    })
-    res.json(logDoc)
+  } else {
+    return res.status(400).json({ error: 'Wrong credentials' });
+  }
 });
 
-app.get('/log', async(req,res)=>{
-    res.json(
-        await Logs.find()
-          .sort({createdAt: -1})
-      );
-})
+app.get('/profile', (req, res) => {
+  const { token } = req.cookies;
+
+  if (!token) {
+    return res.status(401).json({ error: 'Unauthorized: Token not provided' });
+  }
+
+  jwt.verify(token, secret, {}, (err, info) => {
+    if (err) {
+      console.error('Token verification error:', err);
+      if (err.name === 'JsonWebTokenError') {
+        return res.status(401).json({ error: 'Unauthorized: Invalid token' });
+      } else {
+        return res.status(500).json({ error: 'Internal Server Error' });
+      }
+    }
+
+    console.log('Decoded user information:', info);
+
+    res.json(info);
+  });
+});
+
+app.post('/logout', (req, res) => {
+  res.cookie('token', '').json('ok');
+});
+
+app.post('/log', upload.none(), async (req, res) => {
+  const { time, duration, region, sensorID, stoppage, profile, comment, measure } = req.body;
+  const logDoc = await Logs.create({
+    time,
+    duration,
+    region,
+    sensorID,
+    stoppage,
+    profile,
+    comment,
+    measure
+  });
+  res.json(logDoc);
+});
+
+app.get('/log', async (req, res) => {
+  res.json(
+    await Logs.find()
+      .sort({ createdAt: -1 })
+  );
+});
 
 app.delete('/log/:id', async (req, res) => {
-    const logId = req.params.id;
-    try {
-        const deletedLog = await Logs.findByIdAndDelete(logId);
+  const logId = req.params.id;
+  try {
+    const deletedLog = await Logs.findByIdAndDelete(logId);
 
-        if (deletedLog) {
-            res.json({ message: 'Log deleted successfully', log: deletedLog });
-        } else {
-            res.status(404).json({ message: 'Log not found' });
-        }
-    } catch (error) {
-        console.error('Error deleting log:', error);
-        res.status(500).json({ message: 'Internal server error' });
+    if (deletedLog) {
+      res.json({ message: 'Log deleted successfully', log: deletedLog });
+    } else {
+      res.status(404).json({ message: 'Log not found' });
     }
+  } catch (error) {
+    console.error('Error deleting log:', error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
 });
 
 app.get('/log/:id', async (req, res) => {
-    try {
-      const log = await Logs.findById(req.params.id);
-      res.json(log);
-    } catch (error) {
-      res.status(500).json({ error: 'Error fetching log data' });
-    }
-  });
+  try {
+    const log = await Logs.findById(req.params.id);
+    res.json(log);
+  } catch (error) {
+    res.status(500).json({ error: 'Error fetching log data' });
+  }
+});
 
+app.put('/log/:id', async (req, res) => {
+  const logId = req.params.id;
+  const updatedLog = req.body;
 
-  app.put('/log/:id', async (req, res) => {
-    const logId = req.params.id;
-    const updatedLog = req.body;
-  
-    try {
-      const updatedLogResult = await Logs.findByIdAndUpdate(req.params.id, req.body, { new: true });
-      res.status(200).json(updatedLogResult);
-    } catch (error) {
-      console.error('Error updating log:', error);
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
-  });
-  
+  try {
+    const updatedLogResult = await Logs.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    res.status(200).json(updatedLogResult);
+  } catch (error) {
+    console.error('Error updating log:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
 
 app.listen(4000);
